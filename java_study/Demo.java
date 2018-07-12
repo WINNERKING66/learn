68ce563c5ab2f4f34e5a70d0b931eff6fb47219c
MountainLi <2350287521@qq.com>
2018-07-12 09:16:51 +0800

    新增demo

---
+++ java_study/Demo.java
@@ -0,0 +1,12 @@
+package java_study;
+
+public class Demo{
+    public Demo(String name){
+        //这个构造器仅有一个参数：name
+        System.out.println("小狗的名字是 : " + name );
+    }
+    public static void main(String []args){
+        // 下面的语句将创建一个Puppy对象
+        Demo myPuppy = new Demo( "tommy" );
+    }
+}
